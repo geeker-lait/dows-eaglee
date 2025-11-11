9392807bdb3ab06367f2a7ec595906c3493bee3c
AutoCommit <auto@local>
2025-11-12 00:32:07 +0800

    auto commit

---
+++ eaglee-app/src/main/java/org/dows/eaglee/EagleeApplication.java
@@ -0,0 +1,18 @@
+package ;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class EagleeApplication{
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+            .directory(System.getProperty("user.home"))
+            .ignoreIfMissing()
+            .load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        SpringApplication.run(EagleeApplication.class, args);
+    }
+}
