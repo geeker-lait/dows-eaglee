c535b2469d1dce7efd92f02501fb7842eb40cd36
AutoCommit <auto@local>
2025-11-12 21:32:45 +0800

    auto commit

--- eaglee-dao/src/main/resources/flyway/mysql/V1.0.251105__create_hina_eaglee.sql
+++ eaglee-dao/src/main/resources/flyway/mysql/V1.0.251105__create_hina_eaglee.sql
@@ -4,7 +4,7 @@ USE `hina_eaglee`;
 
 drop table if exists `task_define`;
 CREATE TABLE IF NOT EXISTS `task_define`(
-    `task_define_id` bigint(19) DEFAULT NULL COMMENT '任务定义ID',
+    `task_define_id` bigint(19) NOT NULL COMMENT '任务定义ID',
     `project_idenfifier` varchar(64) DEFAULT NULL COMMENT '项目标识',
     `task_identifier` varchar(64) DEFAULT NULL COMMENT '任务标识(表明同一类任务的标识)',
     PRIMARY KEY (`task_define_id`)
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS `task_define`(
 
 drop table if exists `task_project`;
 CREATE TABLE IF NOT EXISTS `task_project`(
-    `task_project_id` bigint(19) DEFAULT NULL COMMENT '任务项目ID',
+    `task_project_id` bigint(19) NOT NULL COMMENT '任务项目ID',
     `project_name` varchar(64) DEFAULT NULL COMMENT '项目名(业务系统中定义的名称)',
     `process_code` varchar(64) DEFAULT NULL COMMENT 'DS中流程编码',
     `project_idenfifier` varchar(64) DEFAULT NULL COMMENT '项目标识',
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS `task_project`(
 
 drop table if exists `task_instance`;
 CREATE TABLE IF NOT EXISTS `task_instance`(
-    `task_instance_id` bigint(19) DEFAULT NULL COMMENT '任务实例ID',
+    `task_instance_id` bigint(19) NOT NULL COMMENT '任务实例ID',
     `task_project_id` bigint(19) DEFAULT NULL COMMENT '任务项目ID',
     `task_name` varchar(64) DEFAULT NULL COMMENT '任务名(ds中的任务编码)',
     `task_identifier` varchar(64) DEFAULT NULL COMMENT '任务标识(表明同一类任务的标识)',
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `task_instance`(
 
 drop table if exists `task_counter`;
 CREATE TABLE IF NOT EXISTS `task_counter`(
-    `task_counter_id` bigint(19) DEFAULT NULL COMMENT '任务计数器ID',
+    `task_counter_id` bigint(19) NOT NULL COMMENT '任务计数器ID',
     `project_idenfifier` varchar(64) DEFAULT NULL COMMENT '项目标识',
     `task_identifier` varchar(64) DEFAULT NULL COMMENT '任务标识',
     `expend_total` bigint(19) DEFAULT NULL COMMENT '总耗时',
@@ -53,7 +53,7 @@ CREATE TABLE IF NOT EXISTS `task_counter`(
 
 drop table if exists `task_runtime`;
 CREATE TABLE IF NOT EXISTS `task_runtime`(
-    `task_runtime_id` bigint(19) DEFAULT NULL COMMENT '任务运行时ID',
+    `task_runtime_id` bigint(19) NOT NULL COMMENT '任务运行时ID',
     `ip` varchar(64) DEFAULT NULL COMMENT '节点IP地址',
     `cpu_usage` integer(11) DEFAULT NULL COMMENT 'cpu用量',
     `mem_usage` integer(11) DEFAULT NULL COMMENT 'mem用量',
@@ -68,7 +68,7 @@ CREATE TABLE IF NOT EXISTS `task_runtime`(
 
 drop table if exists `task_metric`;
 CREATE TABLE IF NOT EXISTS `task_metric`(
-    `task_metric_id` bigint(19) DEFAULT NULL COMMENT '任务度量ID',
+    `task_metric_id` bigint(19) NOT NULL COMMENT '任务度量ID',
     `ip` varchar(64) DEFAULT NULL COMMENT '节点IP地址',
     `time_unit` integer(11) DEFAULT NULL COMMENT '时间单位(分钟，小时，天)',
     `cpu_total_usage` integer(11) DEFAULT NULL COMMENT 'cpu使用总量',
@@ -82,7 +82,7 @@ CREATE TABLE IF NOT EXISTS `task_metric`(
 
 drop table if exists `task_config`;
 CREATE TABLE IF NOT EXISTS `task_config`(
-    `task_config_id` bigint(19) DEFAULT NULL COMMENT '任务配置ID',
+    `task_config_id` bigint(19) NOT NULL COMMENT '任务配置ID',
     `key` varchar(64) DEFAULT NULL COMMENT '键名[采集间隔(不能超过60秒且能被60整除），度量单位[分钟,小时,天]，统计IP结合......]',
     `deescription` varchar(64) DEFAULT NULL COMMENT '描述',
     `data_type` varchar(64) DEFAULT NULL COMMENT '数据类型',
@@ -99,7 +99,7 @@ CREATE TABLE IF NOT EXISTS `task_config`(
 
 drop table if exists `task_rule`;
 CREATE TABLE IF NOT EXISTS `task_rule`(
-    `task_rule_id` bigint(19) DEFAULT NULL COMMENT '任务规则ID',
+    `task_rule_id` bigint(19) NOT NULL COMMENT '任务规则ID',
     `rule_name` varchar(64) DEFAULT NULL COMMENT '规则名',
     `config_json` varchar(64) DEFAULT NULL COMMENT 'json配置',
     `reference_type` integer(11) DEFAULT NULL COMMENT '应用类型(0:项目,1:任务)',
@@ -115,7 +115,7 @@ CREATE TABLE IF NOT EXISTS `task_rule`(
 
 drop table if exists `task_setting`;
 CREATE TABLE IF NOT EXISTS `task_setting`(
-    `task_setting_id` bigint(19) DEFAULT NULL COMMENT '任务设置ID',
+    `task_setting_id` bigint(19) NOT NULL COMMENT '任务设置ID',
     `task_rule_id` bigint(19) DEFAULT NULL COMMENT '任务规则ID',
     `workflow_idenfifier` varchar(64) DEFAULT NULL COMMENT '项目标识',
     `task_identifier` varchar(64) DEFAULT NULL COMMENT '任务标识(表明同一类任务的标识)',
